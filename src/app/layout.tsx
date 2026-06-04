e261fc9902a2915eccbb59348e27b63ca3c8941f
andrewrussellagency-sketch <andrewrussellagency@gmail.com>
2026-06-04 12:26:59 +0100

    Improved homepage, US locations, dark mode, live chat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -93,19 +93,23 @@ export default function RootLayout({
         {children}
 
         {/* Tawk.to Live Chat Widget */}
-     <!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/6a0cfd0fbd282b1c34fd815c/1jp1bp2av';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
+        <Script
+          id="tawkto"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/YOUR_TAWK_ID/default';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
