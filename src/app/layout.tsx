fc2a241be04211fd59b33e4df6d0301cb24f1e96
andrewrussellagency-sketch <andrewrussellagency@gmail.com>
2026-06-04 13:03:15 +0100

    Add Tawk.to live chat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -92,24 +92,19 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
 
-        {/* Tawk.to Live Chat Widget */}
-        <Script
-          id="tawkto"
-          strategy="lazyOnload"
-          dangerouslySetInnerHTML={{
-            __html: `
-              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-              (function(){
-                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-                s1.async=true;
-                s1.src='https://embed.tawk.to/YOUR_TAWK_ID/default';
-                s1.charset='UTF-8';
-                s1.setAttribute('crossorigin','*');
-                s0.parentNode.insertBefore(s1,s0);
-              })();
-            `,
-          }}
-        />
+       <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+   (function(){
+   var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+   s1.async=true;
+   s1.src='https://embed.tawk.to/6a216839b8952c1c325ba3b5/1jq97u8tq';
+   s1.charset='UTF-8';
+   s1.setAttribute('crossorigin','*');
+   s0.parentNode.insertBefore(s1,s0);
+   })();
+</script>
+<!--End of Tawk.to Script-->
       </body>
     </html>
   );
